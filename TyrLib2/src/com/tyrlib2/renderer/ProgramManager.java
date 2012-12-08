818f13bf7c81edfb6f17f6bb8f241ecbfc3d2a03
TyrfingX <saschamueller@gmx.net>
2012-12-08 11:41:38 +0100

    Compiling of vertex/fragment shaders and programs

---
+++ TyrLib2/src/com/tyrlib2/renderer/ProgramManager.java
@@ -0,0 +1,68 @@
+package com.tyrlib2.renderer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import android.opengl.GLES20;
+
+/**
+ * This class takes care of linking and compiling programs and cashing the result
+ * @author Sascha
+ *
+ */
+
+public class ProgramManager {
+	
+	/** Singleton instance **/
+	private static ProgramManager instance;
+
+	/** Compiled programs **/
+	private Map<String, Integer> programs;
+	
+	public ProgramManager() {
+		programs = new HashMap<String, Integer>();
+	}
+	
+	/**
+	 * Gets an instance to this singleton object
+	 * @return Instance to this singleton object
+	 */
+	public static ProgramManager getInstance() {
+		if (instance == null) {
+			instance = new ProgramManager();
+		} 
+		
+		return instance;
+	}
+	
+	/**
+	 * Creates a new OpenGL program using a vertex and a fragment shader
+	 * @param programName			Name of the new program
+	 * @param vertexShaderName		Name of the vertex shader
+	 * @param fragmentShaderName	Name of the fragment shader
+	 * @return						A handle to the created program
+	 */
+	
+	public int createProgram(String programName, String vertexShaderName, String fragmentShaderName) {
+        int vertexShader = ShaderManager.getInstance().getShader(vertexShaderName);
+        int fragmentShader = ShaderManager.getInstance().getShader(fragmentShaderName);
+
+        int program = GLES20.glCreateProgram();             // create empty OpenGL ES Program
+        GLES20.glAttachShader(program, vertexShader);   	// add the vertex shader to program
+        GLES20.glAttachShader(program, fragmentShader); 	// add the fragment shader to program
+        GLES20.glLinkProgram(program);                  	// creates OpenGL ES program executables
+		
+        programs.put(programName, program);
+		return program;
+	}
+	
+	/**
+	 * Gets a compiled OpenGL program
+	 * @param programName	Name of the program
+	 * @return				A handle to the compiled program
+	 */
+	
+	public int getProgram(String programName) {
+		return programs.get(programName);
+	}
+}
--- TyrLib2/src/com/tyrlib2/renderer/ShaderManager.java
+++ TyrLib2/src/com/tyrlib2/renderer/ShaderManager.java
@@ -1,17 +1,22 @@
 package com.tyrlib2.renderer;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import android.opengl.GLES20;
+
 /** 
- * Takes care of loading vertex and fragment programs and compiling them
- * and cashing the compiled shader.
+ * Takes care of loading vertex and fragment shaders and cashing them
  * @author Sascha
  *
  */
 
 public class ShaderManager {
 	private static ShaderManager instance;
+	private Map<String, Integer> shaders;
 	
 	public ShaderManager() {
-		
+		shaders = new HashMap<String, Integer>();
 	}
 	
 	/**
@@ -26,4 +31,37 @@ public class ShaderManager {
 		
 		return instance;
 	}
+	
+	/**
+	 * Loads a shader
+	 * @param name
+	 * @param type
+	 * @param shaderCode
+	 * @return
+	 */
+	
+    public int loadShader(String name, int type, String shaderCode){
+
+        // create a vertex shader type (GLES20.GL_VERTEX_SHADER)
+        // or a fragment shader type (GLES20.GL_FRAGMENT_SHADER)
+        int shader = GLES20.glCreateShader(type);
+
+        // add the source code to the shader and compile it
+        GLES20.glShaderSource(shader, shaderCode);
+        GLES20.glCompileShader(shader);
+
+        shaders.put(name, shader);
+        
+        return shader;
+    }
+    
+    /**
+     * Gets a shader
+     * @param name	Name of the shader
+     * @return		A handle to the shader
+     */
+    
+    public int getShader(String name) {
+    	return shaders.get(name);
+    }
 }
