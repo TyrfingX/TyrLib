8e4139169bfcf00859c414c7192ee6bd476504fe
TyrfingX <saschamueller@gmx.net>
2012-12-08 09:36:39 +0100

    Added file reading & writing

---
+++ TyrLib2/src/com/tyrfing/tyrlib2/files/FileReader.java
@@ -0,0 +1,69 @@
+package com.tyrfing.tyrlib2.files;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import android.content.Context;
+
+/**
+ * Reads a textfile belonging to a Context (Activity usually)
+ * @author Sascha
+ *
+ */
+
+public class FileReader {
+	
+	/**
+	 * Reads a textfile.
+	 * @param context 	The instance of the activity class.
+	 * @param name		Name of the file to be read
+	 * @return			A String representation of the file.
+	 */
+	
+	public static String readFile(Context context, String name)
+	{
+		FileInputStream fis;
+		try {
+			fis = context.openFileInput(name);
+			int ch;
+			StringBuffer fileContent = new StringBuffer();
+			while( (ch = fis.read()) != -1)
+				  fileContent.append((char)ch);
+			
+			fis.close();
+			
+			return new String(fileContent);
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	
+		return "";
+	
+	}
+	
+	/**
+	 * Checks if a file exists.
+	 * @param context 	The instance of the activity class.
+	 * @param name		Name of the file to be read
+	 * @return			True iff the file exists.
+	 */
+	
+	public static boolean fileExists(Context context, String fileName)
+	{
+		FileInputStream fis;
+		try {
+			fis = context.openFileInput(fileName);
+			fis.close();
+			return true;
+		} catch (FileNotFoundException e) {			
+		} catch (IOException e) {
+		}
+		
+		return false;
+	}
+}
---
+++ TyrLib2/src/com/tyrfing/tyrlib2/files/FileWriter.java
@@ -0,0 +1,41 @@
+package com.tyrfing.tyrlib2.files;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import android.content.Context;
+
+/**
+ * Writes content to a file. The file is associated with a context,
+ * usually an activity.
+ * @author Sascha
+ *
+ */
+
+public class FileWriter {
+	
+	/**
+	 * Writes a string representation of the data into the file
+	 * @param context	The instance of the activity class.
+	 * @param name		The name of the file.
+	 * @param value		The content to be written into the file.
+	 */
+	
+	public static void writeFile(Context context, String name, String value)
+	{	
+		FileOutputStream fos;
+		try {
+			fos = context.openFileOutput(name, Context.MODE_PRIVATE);
+			fos.write(value.getBytes());
+			fos.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+}
