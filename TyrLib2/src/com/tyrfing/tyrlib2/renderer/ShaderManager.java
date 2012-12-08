b249517f620ed19f31217be09144132876e86a2b
TyrfingX <saschamueller@gmx.net>
2012-12-08 09:43:52 +0100

    Added Shader Manager.

---
+++ TyrLib2/src/com/tyrfing/tyrlib2/renderer/ShaderManager.java
@@ -0,0 +1,24 @@
+package com.tyrfing.tyrlib2.renderer;
+
+/** 
+ * Takes care of loading vertex and fragment programs and compiling them
+ * and cashing the compiled shader.
+ * @author Sascha
+ *
+ */
+
+public class ShaderManager {
+	private static ShaderManager instance;
+	
+	public ShaderManager() {
+		
+	}
+	
+	public static ShaderManager getInstance() {
+		if (instance == null) {
+			instance = new ShaderManager();
+		} 
+		
+		return instance;
+	}
+}
