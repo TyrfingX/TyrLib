fe81507071c50769fa070dcf2b1b190708656c9b
TyrfingX <saschamueller@gmx.net>
2013-02-27 20:19:44 +0100

    Added joysticks

---
+++ TyrLib2/src/com/tyrlib2/input/Joystick.java
@@ -0,0 +1,118 @@
+package com.tyrlib2.input;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.view.MotionEvent;
+
+import com.tyrlib2.math.Vector2;
+
+/**
+ * A basic joystick for managing user input
+ * @author Sascha
+ *
+ */
+
+
+public class Joystick implements ITouchListener {
+
+	/** Enable the joystick per default **/
+	private boolean enabled = true;
+	
+	/** Set the joysticks priority to the lowest level by default **/
+	private long priority = 0;
+	
+	/** Whether or not the joystick is currently being used or not **/
+	private boolean active = false;
+	
+	/** The center of the joystick **/
+	private Vector2 basePoint;
+	
+	/** The listeners of this joystick **/
+	private List<JoystickListener> listeners;
+	
+	/** The id of the finger which is currently controlling this joystick **/
+	private int fingerId;
+	
+	public Joystick() {
+		listeners = new ArrayList<JoystickListener>();
+	}
+	
+	@Override
+	public long getPriority() {
+		return priority;
+	}
+
+
+	/** If the user starts touching an point then initialize the joystick with
+	 * 	the touched position as base state
+	 */
+	@Override
+	public boolean onTouchDown(Vector2 point, MotionEvent event) {
+		if (!active) {
+			active = true;
+			basePoint = point;
+			fingerId = event.getActionIndex();
+			
+			for (int i = 0; i < listeners.size(); ++i) {
+				listeners.get(i).onJoystickActivated();
+			}
+		}
+		return true;
+	}
+
+	/**	The user stopped using the joystick, assuming this is the 
+	 * 	right touch stopping (considering multi touch)
+	 */
+	@Override
+	public boolean onTouchUp(Vector2 point, MotionEvent event) {
+		if (active && event.getActionIndex() == fingerId) {
+			active = false;
+			for (int i = 0; i < listeners.size(); ++i) {
+				listeners.get(i).onJoystickDeactivated();
+			}
+		}
+		return true;
+	}
+
+	
+	/**	Use the joystick
+	 * 	if its currently active
+	 */
+	@Override
+	public boolean onTouchMove(Vector2 point, MotionEvent event) {
+		if (active && event.getActionIndex() == fingerId) {
+			for (int i = 0; i < listeners.size(); ++i) {
+				listeners.get(i).onJoystickMoved(basePoint.vectorTo(point));
+			}
+		}
+		return true;
+	}
+	
+	@Override
+	public boolean isEnabled() {
+		return enabled;
+	}
+	
+	public void addListener(JoystickListener listener) {
+		listeners.add(listener);
+	}
+	
+	public void removeListener(JoystickListener listener) {
+		listeners.remove(listener);
+	}
+
+	public Vector2 getBasePoint() {
+		return basePoint;
+	}
+
+	public void setEnabled(boolean enabled) {
+		this.enabled = enabled;
+	}
+
+	public void setPriority(long priority) {
+		this.priority = priority;
+	}
+	
+
+}
---
+++ TyrLib2/src/com/tyrlib2/input/JoystickListener.java
@@ -0,0 +1,21 @@
+package com.tyrlib2.input;
+
+import com.tyrlib2.math.Vector2;
+
+/**
+ * An interface for listeners of joysticks
+ * @author Sascha
+ *
+ */
+
+public interface JoystickListener {
+	
+	/** The user started using the joystick **/
+	public void onJoystickActivated();
+	
+	/** The user stopped using the joystick **/
+	public void onJoystickDeactivated();
+	
+	/** The joystick was moved. movement is the relative point to the basepoint of the joystick **/
+	public void onJoystickMoved(Vector2 movement);
+}
