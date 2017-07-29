87df04a906d2fbf2776f52e333bae80105a96ea8
TyrfingX <saschamueller@gmx.net>
2017-07-29 09:36:48 +0200

    1: AI Heuristic
    Implemented simple heuristic:
    
    - Sum up the HP of the own units
    - Sum up total HP of all units
    - evaluation = ownHp / totalHp (making it a number between 0 and 1)
    - if the game state is a winning game state
    evaluation = 1 + 1 / #moves
    guarantees that a winning move with the least number of moves is
    preferably chosen
    
    Task-Url: https://github.com/TyrfingX/TyrLib/issues/1

--- com.tyrfing.games.id18/src/com/tyrfing/games/id18/model/ai/Heuristic.java
+++ com.tyrfing.games.id18/src/com/tyrfing/games/id18/model/ai/Heuristic.java
@@ -2,6 +2,8 @@ package com.tyrfing.games.id18.model.ai;
 
 import com.tyrfing.games.id18.model.battle.Battle;
 import com.tyrfing.games.id18.model.unit.Faction;
+import com.tyrfing.games.id18.model.unit.StatType;
+import com.tyrfing.games.id18.model.unit.Unit;
 
 public class Heuristic {
 	
@@ -15,10 +17,23 @@ public class Heuristic {
 	
 	public float getEvaluation(int depth) {
 		if (battle.areObjectivesAchieved(faction)) {
-			return 1.f / depth;
+			// This is a winning state, we should definitely take it,
+			// only consideration is that we should take it 
+			return 1 + 1f / depth;
 		} 
 		
-		return 0;
+		float evaluation = 0;
+		float maxEvaluation = 0;
+		
+		for (Unit unit : battle.getWaitingUnits()) {
+			int hp = unit.getStats().get(StatType.HP);
+			if (unit.getFaction().equals(faction)) {
+				evaluation += hp;
+			} 
+			maxEvaluation += hp;
+		}
+		
+		return evaluation / maxEvaluation;
 	}
 	
 	public Faction getFaction() {
